c499fe07b6abd56d5d310db98c6ab0ce8ff05bbe
Daniel Cloud <koruptpaintbaler@gmail.com>
2017-09-21 18:23:27 -0500

    Start of White Birch Adventures

---
+++ src/main/java/com/wba/wba/WbaApplication.java
@@ -0,0 +1,12 @@
+package com.wba.wba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbaApplication.class, args);
+	}
+}
