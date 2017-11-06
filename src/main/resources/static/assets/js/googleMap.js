1c58e0b93278bb9947ee833aaf48f3e1344ad1aa
Daniel <koruptpaintbaler@gmail.com>
2017-11-05 19:02:43 -0600

    beautification in progress

--- src/main/resources/static/assets/js/googleMap.js
+++ src/main/resources/static/assets/js/googleMap.js
@@ -1,7 +1,7 @@
 function myMap() {
-    var location = {lat: 33.104277, lng: -96.6508143};
+    var location = {lat: 44.9364212, lng: -84.3996779};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
+        zoom: 15,
         center: location
     });
     var marker = new google.maps.Marker({
